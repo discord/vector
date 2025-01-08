a35ab9864d199ca6edca43fb6bef8d76b8342062
Alexander Jiang <alexander.jiang@discordapp.com>
2025-01-08 02:11:39 +0000

    Return non-zero exit code for non graceful shutdown (#8)

--- lib/vector-buffers/src/variants/disk_v2/reader.rs
+++ lib/vector-buffers/src/variants/disk_v2/reader.rs
@@ -977,6 +977,7 @@ where
             if self.ledger.is_writer_done() {
                 let total_buffer_size = self.ledger.get_total_buffer_size();
                 if total_buffer_size == 0 {
+                    debug!("buffer writer is done and buffer is empty");
                     return Ok(None);
                 }
             }
@@ -1074,6 +1075,7 @@ where
                     continue;
                 }
 
+                debug!("waiting for buffer writer to wake the reader");
                 self.ledger.wait_for_writer().await;
             } else {
                 debug!(
--- src/topology/running.rs
+++ src/topology/running.rs
@@ -152,9 +152,10 @@ impl RunningTopology {
                     components = ?remaining_components,
                     "Failed to gracefully shut down in time. Killing components."
                 );
-            }) as future::BoxFuture<'static, ()>
+                Result::Err(remaining_components)
+            }) as future::BoxFuture<'static, Result<(), String>>
         } else {
-            Box::pin(future::pending()) as future::BoxFuture<'static, ()>
+            Box::pin(future::pending()) as future::BoxFuture<'static, Result<(), String>>
         };
 
         // Reports in intervals which components are still running.
@@ -190,19 +191,24 @@ impl RunningTopology {
         };
 
         // Finishes once all tasks have shutdown.
-        let success = futures::future::join_all(wait_handles).map(|_| ());
+        let success = futures::future::join_all(wait_handles).map(|_| Result::Ok(()));
 
         // Aggregate future that ends once anything detects that all tasks have shutdown.
         let shutdown_complete_future = future::select_all(vec![
-            Box::pin(timeout) as future::BoxFuture<'static, ()>,
-            Box::pin(reporter) as future::BoxFuture<'static, ()>,
-            Box::pin(success) as future::BoxFuture<'static, ()>,
+            Box::pin(timeout) as future::BoxFuture<'static, Result<(), String>>,
+            Box::pin(reporter.map(|()| Result::<(), String>::Ok(()))) as future::BoxFuture<'static, Result<(), String>>,
+            Box::pin(success) as future::BoxFuture<'static, Result<(), String>>,
         ]);
 
         // Now kick off the shutdown process by shutting down the sources.
-        let source_shutdown_complete = self.shutdown_coordinator.shutdown_all(deadline);
+        let source_shutdown_complete = self.shutdown_coordinator.shutdown_all(deadline).map(|_| Result::<(), String>::Ok(()));
 
-        futures::future::join(source_shutdown_complete, shutdown_complete_future).map(|_| ())
+        // Panic to ensure that Vector returns a non-zero exit code when it's unable to gracefully shutdown
+        futures::future::join(source_shutdown_complete, shutdown_complete_future)
+            .map(|futures| match futures.1.0 {
+                Result::Err(s) => panic!("failed to gracefully shutdown in time, panic to force non-zero exit code. remaining components: {}", &s),
+                Result::Ok(_) => (),
+            })
     }
 
     /// Attempts to load a new configuration and update this running topology.
